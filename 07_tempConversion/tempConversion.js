1e1a504aede163e07866b4589b21b734d877e212
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-05-04 14:53:42 -0400

    Round to 1 decimal with Math.round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let t = (temp - 32) * 5 / 9;
+  if (t % 1 !== 0) t = Math.round(t * 10) / 10
+
+  return t;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let t = (temp * 9 / 5) + 32;
+  if (t % 1 !== 0) t = Math.round(t * 10) / 10
+
+  return t;
 };
 
 // Do not edit below this line
