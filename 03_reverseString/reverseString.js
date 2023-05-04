135861d258c67ce8da0b411db275012446d32dc8
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-05-04 13:23:01 -0400

    Create loop to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let len = str.length;
+    let string = '';
+    
+    for (i = len - 1; i >= 0; i--) {
+        string += str[i];
+    }
 
+    return string;
 };
 
 // Do not edit below this line
