40610a2868a8633a421f75d0c898ca237e07b534
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-05-04 14:28:19 -0400

    Used sum loop and typeof variable check

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (typeof n1 !== "number" || typeof n2 !== "number" || n1 < 0 || n2 < 0) return "ERROR";
 
+    let sum = 0, x1, x2, temp;
+
+    if (n1 > n2) {
+        temp = n1;
+        n1 = n2;
+        n2 = temp;
+    } 
+
+    for (let i = n1; i <= n2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
