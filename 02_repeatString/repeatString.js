9e4b649b8e7bab896a45c4a3421c234e22915ec2
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-05-04 13:11:49 -0400

    Create repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let string = str;
 
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        return string.repeat(num);
+    }
 };
 
 // Do not edit below this line
