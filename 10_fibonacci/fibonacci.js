ff105d9e3036f043ab5a47da573875780f7eb8fb
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-06-26 15:35:41 -0400

    Create fibonacci loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number < 0) return "OOPS";
+  
+  var fib = [1, 1];
+  for (let i = 2; i < number; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[number - 1];
 };
 
 // Do not edit below this line
