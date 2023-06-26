f8ac83b6ce609de9e75fde1918f7b73a71140f77
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-06-26 15:22:09 -0400

    Reverse string to check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const newString = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  const reversedString = newString.split('').reverse().join('');
+  return reversedString == newString;
 };
 
 // Do not edit below this line
